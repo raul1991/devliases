616125c2deb73ace3e709243918b6bb8bb0e1cff
Rahul Bawa <Rahul Bawa rahul.bawa@ericsson.com>
2019-10-01 12:20:49 +0530

    Added more aliases for kubectl

--- dev_aliases.sh
+++ dev_aliases.sh
@@ -1,35 +1,51 @@
 #!/bin/bash
 
-# docker related aliases
-alias _di='docker image ls'
-alias _dni='docker image ls | head -n'
-alias _dc='docker container ps'
-alias _dnc='docker container ps | head -n'
-alias _dv='docker volume ls'
-alias _dnv='docker volume ls | head -n'
-alias _dn='docker network ls'
-alias _dnn='docker network ls | head -n'
-
+# sudo docker related aliases
+alias _d='sudo docker'
+alias _di='sudo docker image ls'
+alias _dni='sudo docker image ls | head -n'
+alias _dc='sudo docker container ps'
+alias _dnc='sudo docker container ps | head -n'
+alias _dv='sudo docker volume ls'
+alias _dnv='sudo docker volume ls | head -n'
+alias _dn='sudo docker network ls'
+alias _dnn='sudo docker network ls | head -n'
+alias _dt='sudo docker tag'
+alias _dp='sudo docker push'
 
 # more pattern specific aliases
-alias dip='docker image ls | grep'
-alias dcp='docker container ls | grep'
+alias _dip='sudo docker image ls | grep'
+alias _dcp='sudo docker container ls | grep'
 
 _dbi()
 {
- docker build -t $1 .
+ sudo docker build "$@"
 }
 
 # kubernetes specific aliases
-
+alias _k='kubectl'
 alias _kgp='kubectl get pods'
 alias _kgd='kubectl get deployments'
 alias _kgs='kubectl get services'
 alias _kgcm='kubectl get configmaps'
+alias _kg='kubectl get'
+alias _kdesc='kubectl describe'
+alias _kexec='kubectl exec -ti'
+alias _kl='kubectl logs'
+
+_kportfwd()
+{
+ kubectl port-forward "$1" "$2"
+}
+
+_kexpose()
+{
+ kubectl expose deployment/$1 --type="$2" --port $3
+}
 
 _kcd()
 {
- kubectl create deployment/$1 --name=$2 --image=$3
+ kubectl create deployment "$1" --image="$2"
 }
 
 _kdd()
@@ -37,3 +53,22 @@ _kdd()
  kubectl delete deployment/$1
 }
 
+_kccm()
+{
+ kubectl create configmap $1
+}
+
+_kdcm()
+{
+  kubectl delete configmap $1
+}
+
+_kd()
+{
+  kubectl delete "$@"
+}
+
+_ka()
+{
+ kubectl apply "$@"
+}
